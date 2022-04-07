cf0610cda76542a01c65418eb4b2a6118fe77ac2
Yuragranovsky <yuragranovsky@gmail.com>
2022-04-07 21:06:16 +0300

    cw #34

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import timeZones from './config/time-zones';
 
 
 import Timer from './components/Timer';
-
+type ComponentNames = "input" | "timer" | "color"
 function App() {
-const [timeZone, setTimeZone] = React.useState("Asia/Jerusalem")
-  return  <div>
-    <Color color={'red'}></Color>
-    <Timer timeZone={timeZone}></Timer>
-    <InputData timeZones={timeZones} injectTimeZone={setTimeZone}></InputData>
+const [timeZone, setTimeZone] = React.useState("Asia/Jerusalem");
+const [color, setColor] = React.useState("red");
+const [componentName, setComponentName] = React.useState<ComponentNames>('input');
+const mapComponents: Map<ComponentNames, React.ReactNode> = new Map();
+mapComponents.set("color", <Color color={color}></Color>);
+mapComponents.set("input", <InputData timeZones={timeZones} injectTimeZone={setTimeZone}></InputData>)
+mapComponents.set("timer", <Timer timeZone={timeZone}></Timer>)
+
+  return  <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+    {Array.from(mapComponents.keys()).map(k => <button onClick={() => setComponentName(k)}>{k}</button>)}
+    {mapComponents.get(componentName)}
     </div>
 }
 
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,11 +8,16 @@ type Props = {
 const Timer: React.FC<Props> = ({interval, timeZone}) => {
     const [time, setTime] = React.useState(new Date());
     function tic():void {
-        
+        console.log("kuku")
         setTime(new Date());
     }
     useEffect(() => {
-        setInterval(tic, interval || 1000);
+        console.log("timer has been mounted")
+        const intervalId = setInterval(tic, interval || 1000);
+        return () => {
+            clearInterval(intervalId)
+            console.log("unmounting timer")
+        }
     }, [interval])
     
     return <div style={{marginLeft: "10vw"}}>
