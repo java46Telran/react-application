f46b39c475dc8837c31068c759490d6e8c9f1d23
Yuragranovsky <yuragranovsky@gmail.com>
2022-04-14 17:35:02 +0300

    life game service solution

--- src/service/LifeMatrix.ts
+++ src/service/LifeMatrix.ts
@@ -1,16 +1,38 @@
-import { getRandomMatrix } from "../util/random";
-
+function fromAlive(nLives: number): number {
+    return +(nLives === 2 || nLives === 3);//to 1 only if the condition true
+}
+function fromDead(nLives: number): number {
+    return +(nLives === 3);//to 1 only if the condition true
+}
 export default class LifeMatrix {
     constructor(private _numbers: number[][]) {
-
     }
     get numbers() {
         return this._numbers
     }
     nextStep(): number[][] {
-        //TODO write an implementation of the life game algorithm
-        //reminder: firstly create copy for updating cells based on the previous matrix
-        this._numbers = getRandomMatrix(this._numbers.length, this._numbers.length, 0, 1);
+        const numbersCopy: number[][] = JSON.parse(JSON.stringify(this.numbers));
+        for (let i = 0; i < this.numbers.length; i++) {
+            for (let j = 0; j < this.numbers[i].length; j++) {
+                numbersCopy[i][j] = this.getNumber(i, j);
+            }
+        }
+        this._numbers = numbersCopy;
         return this._numbers;
     }
+    private getNumber(i: number, j: number): number {
+        const neighbours: (number|undefined)[] =
+            [...this.rowNeighbours(i-1,j),...this.rowNeighbours(i+1,j),
+                this.numbers[i][j-1], this.numbers[i][j+1]];
+        const nLives = neighbours.reduce((res: number, cur) => {
+            return cur ? res + 1 : res;
+        }, 0)
+
+        return this.numbers[i][j] ? fromAlive(nLives) : fromDead(nLives);
+    }
+    private rowNeighbours(i: number, j: number):(number|undefined)[] {
+       return this.numbers[i]===undefined ? [undefined] : [this.numbers[i][j],
+           this.numbers[i][j-1], this.numbers[i][j+1] ];
+    }
+
 }
