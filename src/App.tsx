a22419d7ac1ef0ed90ee051994fcedb35484f415
Yuragranovsky <yuragranovsky@gmail.com>
2022-05-01 13:08:18 +0300

    HW #39 dispatchRemove and dispatchUpdate inside useImitator

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import {  COURSES_PATH, ROUTES } from './config/routes-config';
 import Navigator from './components/navigators/Navigator';
 import { useImitator } from './util/useImitator';
+import { useSelector } from 'react-redux';
+import { StateType } from './redux/store';
+import { Course } from './models/Course';
 
 const App: React.FC = () => {
+ 
   useImitator();
-  const [flNavigate, setFlNavigate] = React.useState<boolean>(true);
+  const [flNavigate, setFlNavigate] = React.useState<boolean>(true); 
   React.useEffect(() => setFlNavigate(false), [])
 return <BrowserRouter>
 <Navigator items={ROUTES} />
--- src/util/useImitator.ts
+++ src/util/useImitator.ts
@@ -1,24 +1,27 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ImitatorAction, imitatorActions } from "../config/imitator-config";
-import { addCourse } from "../redux/actions";
+import { addCourse, removeCourse, updateCourse } from "../redux/actions";
 import { getRandomCourse } from "./randomCourse";
 import courseData from "../config/courseData.json"
 import { getRandomNumber } from "./random";
 import { useEffect } from "react";
+import { Course } from "../models/Course";
+import { StateType } from "../redux/store";
+let courses: Course[];
 export function useImitator() {
+    courses = useSelector<StateType, Course[]>(state => state.courses);
     const dispatch = useDispatch();
     useEffect(() => {
         const intervalId = setInterval(action, 2000);
-        
         return () => clearInterval(intervalId)
     }, [])
-    
-  
+
+
     function action() {
-        const number = getRandomNumber(1,100);
+        const number = getRandomNumber(1, 100);
         console.log(number)
         const imitatorAction: ImitatorAction = getAction(number);
-        switch(imitatorAction.action) {
+        switch (imitatorAction.action) {
             case 'add': dispatchAdd(); break;
             case 'remove': dispatchRemove(); break;
             case 'update': dispatchUpdate(); break;
@@ -29,13 +32,27 @@ export function useImitator() {
         dispatch(addCourse(getRandomCourse(courseData)));
     }
     function dispatchRemove() {
-      //TODO
+        console.log('removing')
+        if (courses.length != 0) {
+            const index = getRandomNumber(0, courses.length - 1);
+
+            dispatch(removeCourse(courses[index].id));
+            console.log(`removed course with id ${courses[index].id}`)
+        }
+
     }
     function dispatchUpdate() {
-      //TODO
+        console.log('updating');
+        if (courses.length != 0) {
+            const index = getRandomNumber(0, courses.length - 1);
+            const course = getRandomCourse(courseData);
+            course.id = courses[index].id;
+            dispatch(updateCourse(course));
+            console.log(`updated course with id ${courses[index].id}`)
+        }
     }
 }
 
 function getAction(num: number): ImitatorAction {
-   return imitatorActions.find(ia => num <= ia.prob) ?? imitatorActions[imitatorActions.length - 1]
+    return imitatorActions.find(ia => num <= ia.prob) ?? imitatorActions[imitatorActions.length - 1]
 }
