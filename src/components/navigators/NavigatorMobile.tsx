2218d345c2c672e66c05a4abb2f4c6ee2a85053d
Yuragranovsky <yuragranovsky@gmail.com>
2022-05-07 18:46:48 +0300

    hw #40 solution not final

--- src/components/navigators/NavigatorMobile.tsx
+++ src/components/navigators/NavigatorMobile.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { RouteType } from '../../models/RouteType';
+import { Link as RouterLink } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, IconButton, ListItem, Toolbar, Typography, Drawer, List } from '@mui/material';
 const NavigatorMobile: React.FC<{items: RouteType[]}> = ({items}) => {
-    //TODO
-    //write implementation of a Navigator for mobile devices
-    //Navigator should be based on Drawer
-    return <div>
-        <label style={{fontSize: '2em'}}>NavigatorMobile is working {items.length} items</label>
-    </div>
+    
+    const [flOpen, setOpen] = React.useState<boolean>(false);
+   
+   
+    function toggleOpen() {
+        setOpen(!flOpen);
+    }
+    function getListItems(): React.ReactNode {
+        return items.map(i => <ListItem onClick={toggleOpen} component={RouterLink} to={i.path} key={i.path}>{i.label}</ListItem>)
+    }
+    return <AppBar position="fixed">
+        <Toolbar><IconButton onClick={toggleOpen} style={{color: 'white'}}>
+            <MenuIcon/>
+        </IconButton>
+        <Typography >Courses Application</Typography>
+        <Drawer open={flOpen} onClose={toggleOpen} anchor="left">
+            <List>
+                {getListItems()}
+            </List>
+        </Drawer></Toolbar>
+    </AppBar>
 }
 export default NavigatorMobile;
