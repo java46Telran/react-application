80ba7b8859eed5668c6a11e3e39c4f1cb97362d1
Yuragranovsky <yuragranovsky@gmail.com>
2022-05-12 21:11:11 +0300

    CW #42

--- src/components/pages/Courses.tsx
+++ src/components/pages/Courses.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { coursesService } from "../../config/service-config";
 import { Course } from "../../models/Course";
 import { StateType } from "../../redux/store";
-const Courses: React.FC = () =>
-{
+import { DataGrid, GridColumns, GridRowParams, GridActionsCellItem } from '@mui/x-data-grid'
+import { Box, Paper } from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
+import { removeCourse } from "../../redux/actions";
+import CourseForm from "../forms/CourseForm";
+function getActions(actionsFn: (params: GridRowParams)=>JSX.Element[]): GridColumns {
+    const columns: GridColumns = [
+    { field: "name", type: "string", headerName: "Course Name", align: "center", headerAlign: "center", flex:1 },
+    { field: "lecturer", type: "string", headerName: "Lecturer", align: "center", headerAlign: "center", flex: 0.7 },
+    { field: "hours", type: "number", headerName: "Hours", align: "right", headerAlign: "center", flex: 0.5 },
+    { field: "cost", type: "number", headerName: "Cost", align: "right", headerAlign: "center", flex: 0.6 },
+    { field: "openingDate", type: "date", headerName: "Date", align: "center", headerAlign: "center", flex: 0.7 },
+    { field: "actions", type: "actions", flex: 0.5, getActions:actionsFn}
+
+]
+return columns
+}
+
+const Courses: React.FC = () => {
+    const dispatch = useDispatch()
     const courses: Course[] = useSelector<StateType, Course[]>(state => state.courses);
-    return <ul>
-        {courses.map(c => <li key={c.id}>{JSON.stringify(c)}</li>)}
-    </ul>
+    const [isEdit, setEdit] = React.useState(false);
+    function actionsFn(params: GridRowParams): JSX.Element[] {
+        const actionElements: JSX.Element[] = [
+            <GridActionsCellItem label="Remove" onClick={() => dispatch(removeCourse(params.id as number))}
+             icon={<Delete/>}/>,
+             <GridActionsCellItem label="Edit" onClick={() => editFn(params.id as number)} icon={<Edit/>}/>
+        ]
+        return actionElements;
+    }
+    function editFn(id: number) {
+        //TODO 
+        //sets a flag for conditional rendering: unmounting DataGrid and mounting COurseForm with being update course
+        //
+        setEdit(true);
+    }
+    const getActionsCallback = useCallback(getActions, [courses]);
+    const columns = getActionsCallback(actionsFn);
+    return <Box sx={{display: 'flex', justifyContent: 'center' }}><Paper sx={{height: {xs: '70vh', sm: '85vh', md: '80vh'}, width: {xs: '100%', md: '80%'}}}>
+        {isEdit ? <CourseForm submitFn={() => setEdit(false)}/> : <DataGrid rows={courses} columns={columns} />}
+    </Paper></Box>
+
 }
 export default Courses;
